1550b24ab468774cdebd68b2f6ca64d43be3104b
Mahin Lalani <145413376+ml378@users.noreply.github.com>
2025-04-05 23:30:50 -0400

    Update LevelA.cpp

--- hw4/LevelA.cpp
+++ hw4/LevelA.cpp
@@ -1,3 +1,12 @@
+/**
+* Author: Mahin Lalani
+* Assignment: Rise of the AI
+* Date due: 2025-04-05, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
 #include "LevelA.h"
 #include "Utility.h"
 
